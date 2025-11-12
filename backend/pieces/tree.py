677fa57a8aefecffecad66d5940a2701ac0835ff
matthijshulsebos <m.c.hulsebos@gmail.com>
2025-11-12 21:01:04 +0100

    :sparkles: Implement Tree piece

---
+++ backend/pieces/tree.py
@@ -0,0 +1,30 @@
+from .piece import Piece
+from typing import List
+
+
+class Tree(Piece):
+    """
+    Tree piece - 2 points
+    - Can be chopped down by lumberjacks
+    - Acts as obstacle for all other pieces (cannot pass through)
+    - Cannot be moved, only captured
+    """
+
+    @property
+    def name(self):
+        return "Tree"
+
+    @property
+    def symbol(self):
+        return "T"
+
+    @property
+    def points(self):
+        return 2
+
+    def valid_moves(self, board):
+        """
+        Trees cannot move.
+        Return empty list.
+        """
+        return []
